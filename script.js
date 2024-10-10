4cc503336e165e21f032289c3ab782123c4e3d87
SkyTechTalks <skytechtalks01@gmail.com>
2024-10-10 18:33:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function turnOn() {
+       document.getElementById('demo').
+       src = 'on-bulb.jpg';
+   }
+   function turnOff() {
+       document.getElementById('demo').
+       src = 'off bulb.jpg';
+   }
+   
